31d6a51e988d86f8d626099a7bee0364e7e21f0d
carolagille <i7679783@bournemouth.ac.uk>
2016-02-22 14:20:29 +0000

    second commit with new line

--- src/Curve.cpp
+++ src/Curve.cpp
@@ -8,5 +8,5 @@ void TorndoCurve::guideCurve(int _particleTime,int _controllPoint)
 {
     float t = (float)_partcielTime/(float)m_timeUp;
     m_resultPoint = m_minHeight*((t-1)**2)+ _controllPoint * (2*t(1-t)) +m_maxHeight*(t**2);
-
+    //new line
 }
